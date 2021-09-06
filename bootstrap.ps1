4d6a2dd58aadd11dab5797fd5cfa437ffbe6f411
ajf8729 <ajf8729@users.noreply.github.com>
2021-09-06 01:46:26 +0000

    deploy: ajf8729/ajf8729.com-hugo@519e1dccaefb82d270317c0e0136aa348b0623ea

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,28 +1,42 @@
 # Prep folders
-New-Item -Path $env:USERPROFILE -Name ".ssh" -ItemType Directory
-New-Item -Name AJF8729 -Path C:\ -ItemType Directory
-New-Item -Name Git -Path C:\AJF8729\ -ItemType Directory
-Set-Location -Path C:\AJF8729\
+Write-Host "Creating folders..."
+if (-not (Test-Path -Path "$env:USERPROFILE\.ssh")) {
+    New-Item -Path "$env:USERPROFILE\.ssh" -ItemType Directory | Out-Null
+}
+if (-not (Test-Path -Path "$env:SystemDrive\AJF8729\Git")) {
+    New-Item -Path "$env:SystemDrive\AJF8729\Git" -ItemType Directory -Force | Out-Null
+}
+
+Set-Location -Path "$env:SystemDrive\AJF8729"
 
 # Download latest release of DesktopAppInstaller
+Write-Host "Downloading DesktopAppInstaller..."
 $tag = (Invoke-WebRequest -UseBasicParsing "https://api.github.com/repos/microsoft/winget-cli/releases" | ConvertFrom-Json)[0].tag_name
 $download = "https://github.com/microsoft/winget-cli/releases/download/$tag/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 Invoke-WebRequest -UseBasicParsing $download -OutFile "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 
 # Install DesktopAppInstaller
-Add-AppxPackage -Path .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Write-Host "Installing DesktopAppInstaller..."
+Add-AppxPackage -Path "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 
 # Install Git
-winget install git -h
+Write-Host "Installing Git..."
+winget install "Git" -h
 
 # Install VS Code
+Write-Host "Installing VS Code..."
 winget install "Microsoft Visual Studio Code" -h
 
 #Set GIT_SSH
+Write-Host "Setting GIT_SSH..."
 [System.Environment]::SetEnvironmentVariable('GIT_SSH', 'C:\Windows\System32\OpenSSH\ssh.exe', [System.EnvironmentVariableTarget]::Machine)
 
 # Download PowerShell profile
+Write-Host "Downloading PowerShell profile..."
 Invoke-RestMethod -Uri "https://raw.githubusercontent.com/ajf8729/dotfiles/main/Microsoft.PowerShell_profile.ps1" | Out-File -LiteralPath $PROFILE -Force
 
 # Download private key
+Write-Host "Downloading SSH private key..."
 scp ajf@anthonyfontanez.com:.ssh\id_rsa "$env:USERPROFILE\.ssh\id_rsa"
+
+Write-Host "All done!"
