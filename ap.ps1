ef7cb901eda7ee9e3c2ff4950846dace713102dd
ajf8729 <ajf8729@users.noreply.github.com>
2022-05-26 21:11:57 +0000

    deploy: ajf8729/ajf8729.com-hugo@500c278af341dde8d445a4113e915e13b5bf1de3

---
+++ ap.ps1
@@ -0,0 +1,1 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force; Install-PackageProvider -Name NuGet -Force; Install-Script -Name Get-WindowsAutoPilotInfo -Force; Get-WindowsAutoPilotInfo -Online
