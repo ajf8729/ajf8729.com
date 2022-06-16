92c08d5bf2ce593a092e5917922a4cd7ac1c4984
ajf8729 <ajf8729@users.noreply.github.com>
2022-06-16 04:47:56 +0000

    deploy: ajf8729/ajf8729.com-hugo@d02ccf23f87223a15c2e8ebb33ac7648de48c342

--- ap.ps1
+++ ap.ps1
@@ -7,5 +7,8 @@ Install-PackageProvider -Name NuGet -Force | Out-Null
 Write-Host 'Installing Get-WindowsAutoPilotInfo script...' -ForegroundColor Green
 Install-Script -Name Get-WindowsAutoPilotInfo -Force | Out-Null
 
+Write-Host 'Installing WindowsAutopilotIntune module...' -ForegroundColor Green
+Install-Module -Name WindowsAutopilotIntune -Force | Out-Null
+
 Write-Host 'Executing "Get-WindowsAutoPilotInfo -Online"...' -ForegroundColor Green
 Get-WindowsAutoPilotInfo -Online
